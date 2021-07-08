853cfbd380890b32f4351b47137e0e7d7c767194
bjsxt_test <15815404@qq.com>
2021-07-08 10:13:14 +0800

    第二次修改Test.java代码

--- com/bjsxt/Test.java
+++ com/bjsxt/Test.java
@@ -2,6 +2,6 @@ package com.bjsxt;
 
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 }
